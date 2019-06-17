f5a9d93889484e012601bf36cc33adcc7c52b524
abc <abc@gmail.com>
2019-06-17 14:36:03 +0300

    fixing some bugs

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth= async (req,res,next)=>{
     try{
         const token = req.header('Authorization').replace('Bearer ','')
         
-        const code=  jwt.verify(token,process.env.JWT_SECRET)
+        const code=  jwt.verify(token, process.env.JWT_SECRET)
         const user= await User.findOne({'_id':code._id, 'tokens.token':token})
         if(!user){
             throw new Error()
